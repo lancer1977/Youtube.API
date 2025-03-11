e5270aeb380666dc8d9429d6d99b41d9acbff0d8
chris <lancer1977@gmail.com>
2025-03-10 20:02:10 -0400

    commiting what is here.

--- Src/YoutubeQueryServiceExtensions.cs
+++ Src/YoutubeQueryServiceExtensions.cs
@@ -1,9 +1,13 @@
 ﻿using Google.Apis.YouTube.v3.Data;
+using PolyhydraGames.APi.Youtube.Interfaces;
 
 namespace PolyhydraGames.APi.Youtube;
 
 public static class YoutubeQueryServiceExtensions
 {
+    public static IYoutubeUserQuery AsUserQuery(this IYoutubeQuery query, string username) 
+        => new YoutubeQueryUserService(query, username);
+
     public static int ToSeconds(this string duration)
     {
         var time = System.Xml.XmlConvert.ToTimeSpan(duration);
